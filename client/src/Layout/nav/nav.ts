b154ac688de529d598d9929ce863a81a8760366c
mohid1214 <110354042+mohid1214@users.noreply.github.com>
2025-09-28 14:21:24 +0500

    Routing and Initializing and transitions

--- client/src/Layout/nav/nav.ts
+++ client/src/Layout/nav/nav.ts
@@ -1,30 +1,37 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../Core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../Core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink,RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
   protected creds: any = {};
   protected accountService = inject(AccountService);
+  private router = inject(Router);
+  private toast = inject(ToastService);
 
   login() {
     this.accountService.login(this.creds).subscribe({
-      next: response => {
-        console.log(response);
+      next: () => {
+        this.toast.success('Login successful!');
+        this.router.navigateByUrl('/members');
         this.creds = {};
       },
       error: error => {
-        alert(error.error);
+        console.log(error.error);
+        this.toast.error(error.error);
       }
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,47 +1,16 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from "../Layout/nav/nav";
-import { AccountService } from '../Core/services/account-service';
-import { Home } from "../Features/home/home";
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
 import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home, RouterOutlet, NgClass],
+  imports: [Nav, RouterOutlet, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-
-  private accountService = inject(AccountService);
-  private http = inject(HttpClient);
-  protected members = signal<User[]>([])
-  protected title = "Dating App";
+export class App {
   protected router = inject(Router);
 
-  async ngOnInit() {
-    this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
 
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    if (userString) {
-      const user = JSON.parse(userString);
-      this.accountService.currentUser.set(user);
-    }
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>("https://localhost:5001/api/members"));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
