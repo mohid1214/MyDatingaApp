364778e2cb8cb5045f364a1786180ec42b7d376c
mohid1214 <110354042+mohid1214@users.noreply.github.com>
2025-09-29 13:29:55 +0500

    error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,10 @@
 using System.Text;
 using API.Data;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -35,6 +35,7 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseAuthentication();
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { MemberDetailed } from '../Features/members/member-detailed/member-detai
 import { Lists } from '../Features/lists/lists';
 import { Messages } from '../Features/messages/messages';
 import { authGuard } from '../Core/guards/auth-guard';
+import { TestErrors } from '../Features/test-errors/test-errors';
+import { NotFound } from '../Shared/errors/not-found/not-found';
+import { ServerError } from '../Shared/errors/server-error/server-error';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -19,6 +22,8 @@ export const routes: Routes = [
             { path: 'messages', component: Messages },
         ]
     },
+    {path: 'errors', component: TestErrors},
 
-    { path: '**', component: Home }
+    {path: 'server-error', component: ServerError},
+    { path: '**', component: NotFound }
 ];
